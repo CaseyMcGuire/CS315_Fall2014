f6e914c2e7e82c6a86f3200e79cbe473f9811fbf
Casey McGuire <caseymcguire90@gmail.com>
2014-09-09 22:53:49 -0700

    added labels to bar graph

--- cs315-hwk1/grapher.js
+++ cs315-hwk1/grapher.js
@@ -1,5 +1,5 @@
 var datatable = 'assets/pixar.txt'; //the file to load
-var file;//for the file to be loaded
+//var file;//for the file to be loaded
 var graphs;//holds formatted graph data
 var singleGraphArea;//variable that holds the area for single graph
 
@@ -11,11 +11,12 @@ var context; //the drawing context
 function init() {
     canvas = $('#canvas')[0];
     context = canvas.getContext("2d");
-    file = Utils.loadFileText(datatable);//load file contents to object
+    var file = Utils.loadFileText(datatable);//load file contents to object
     file =  file.split(new RegExp("\\n"));
     console.log(file);//print to developer console
     console.log(typeof file);
 
+    //create a method to help 
     file.getData = function(){
 	   var a = new Array();
 	   for(var x = 2; x < file.length; x++){
@@ -61,7 +62,7 @@ function draw() {
     console.log("graph height" + canvas.height);
     //draw our graphs
     drawLineGraph(0, singleGraphArea);
-   // drawBarGraph(singleGraphArea, 2*singleGraphArea);
+    drawBarGraph(singleGraphArea, 2*singleGraphArea);
    // drawPieChart(2*xArea, 3*xArea);
     
     //make them show up on the screen
@@ -79,6 +80,7 @@ function drawSeparator(xTo){
     }
 }
 
+//return an object that contains values that will help scale a graph properly
 function getGraphDimensions(begin, end){
     var sideBuffer = (end - begin) *.05;
 
@@ -93,6 +95,16 @@ function getGraphDimensions(begin, end){
 
     var topOfGraph = canvas.height - topBuffer;
     var bottomOfGraph = canvas.height - bottomBuffer;
+
+    return{
+	begin: begin,
+	end: end,
+	sideBuffer: sideBuffer,
+	bottomBuffer: bottomBuffer,
+	topBuffer: topBuffer,
+	topOfGraph: topOfGraph,
+	bottomOfGraph: bottomOfGraph
+    }
 }
 
 function drawLineGraph(begin, end){
@@ -100,22 +112,15 @@ function drawLineGraph(begin, end){
     //create a buffer of white space between the 
     //graph and the edges of the allocated graph
     //space
-    var sideBuffer = (end - begin) *.05;
-
-    begin += sideBuffer;
-    end -= sideBuffer;
-
-    var bottomBuffer = canvas.height*.85;
-    var topBuffer = canvas.height/6;
-
-    console.log("bottom buffer " + bottomBuffer);
-    console.log("top buffer " + topBuffer);
-
-    topOfGraph = canvas.height - topBuffer;
-    bottomOfGraph = canvas.height - bottomBuffer;
-
-    console.log("top of graph" + topOfGraph);
-    console.log("bottom of graph" + bottomOfGraph)    
+     var dimensions = getGraphDimensions(begin, end);
+    
+    begin = dimensions.begin;
+    end = dimensions.end;
+    var sideBuffer = dimensions.sideBuffer;
+    var bottomBuffer = dimensions.bottomBuffer;
+    var topBuffer = dimensions.topBuffer;
+    var topOfGraph = dimensions.topOfGraph;
+    var bottomOfGraph = dimensions.bottomOfGraph;    
 
     //first draw the axes
     drawAxes(begin, end, topOfGraph, bottomOfGraph);
@@ -141,22 +146,22 @@ function drawLineGraph(begin, end){
     var x;
     var y = graphs.data[0].value;
     for(var i = 1; i <= graphs.data.length ; i++){
-	   x = (i-1)*lineWidth + lineWidth;
-	   y = -((graphs.data[i-1].value - smallest)/(largest - smallest))*height + topOfGraph;
-       if(i == 1) context.moveTo(x, y);
-	   context.lineTo(x, y);
-	   context.arc(x, y, 1, 0, 2*Math.PI, false);
-
+	x = (i-1)*lineWidth + lineWidth;
+	y = -((graphs.data[i-1].value - smallest)/(largest - smallest))*height + topOfGraph;
+	if(i == 1) context.moveTo(x, y);
+	context.lineTo(x, y);
+	context.arc(x, y, 1, 0, 2*Math.PI, false);
+	
     }
     drawLabels(begin,lineWidth);
-
+    
 }
 
 //find the smallest value in the graphs.data array
 //used to help scale the graph
 function findSmallestValue(){
     var smallest = graphs.data[0].value;
-        for(var i = 1; i < graphs.data.length; i++){
+    for(var i = 1; i < graphs.data.length; i++){
         if(smallest > graphs.data[i].value){
             smallest = graphs.data[i].value;
         }
@@ -164,6 +169,8 @@ function findSmallestValue(){
     return smallest;
 }
 
+//find the largest value in the graphs.data array
+//used to help scale the graph
 function findLargestValue(){
     var largest = graphs.data[0].value;
     for(var i = 1; i < graphs.data.length; i++){
@@ -175,15 +182,15 @@ function findLargestValue(){
 }
 
 //draws axes in for a single graph
-function drawAxes(begin, end, topOfGraph, bottomOfGraphs){
+function drawAxes(begin, end, topOfGraph, bottomOfGraph){
     //draw x axis
     context.moveTo(begin, topOfGraph);
     context.lineTo(end, topOfGraph);
-
+    
     //draw y axis
     context.moveTo(begin, bottomOfGraph);
     context.lineTo(begin, topOfGraph);
-
+    
     //create divots along x axis
     context.moveTo(begin, topOfGraph);
     var increment = getLength(begin,end)/graphs.data.length;
@@ -194,15 +201,16 @@ function drawAxes(begin, end, topOfGraph, bottomOfGraphs){
 }
 
 //draws appropriate labels beneath graphs
-function drawLabels(begin, spacing){
+function drawLabels(begin, spacing, topOfGraph){
     context.save();
-    context.translate(begin-10, canvas.height);
+    console.log("begin" + begin);
+    context.translate(begin, canvas.height);
     context.rotate(-Math.PI/2);
-
+       
     var x = spacing;
     for(var i = 0; i< graphs.data.length; i++){
-	   context.fillText(graphs.data[i].label , begin, spacing, 80);
-	   spacing += x;
+	context.fillText(graphs.data[i].label , 20, spacing-10, 80);
+	spacing += x;
     }
     context.restore();
 }
@@ -213,12 +221,27 @@ function getLength(begin, end){
 }
 
 function drawBarGraph(begin, end){
-    var barWidth = singleGraphArea/graphs.data.length;
+
+     var dimensions = getGraphDimensions(begin, end);    
+    begin = dimensions.begin;
+    end = dimensions.end;
+    var sideBuffer = dimensions.sideBuffer;
+    var bottomBuffer = dimensions.bottomBuffer;
+    var topBuffer = dimensions.topBuffer;
+    var topOfGraph = dimensions.topOfGraph;
+    var bottomOfGraph = dimensions.bottomOfGraph; 
+    
+    drawAxes(begin, end, topOfGraph, bottomOfGraph);
+
+
+    var barWidth = getLength(begin,end)/graphs.data.length;
     
     for(var i = 0; i< graphs.data.length; i++){
 	   var barHeight = graphs.data[i].value;
 	   context.fillRect(i*barWidth + begin,500 - barHeight, barWidth, barHeight);
     }
+    console.log("bar width" + barWidth);
+    drawLabels(begin, barWidth);
 }
 
 function drawPieChart(xFrom, xTo){
