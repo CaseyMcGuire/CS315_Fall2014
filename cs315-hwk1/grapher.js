3bcdf630f45816b3bc734629b00c40159c62c8f1
Casey McGuire <caseymcguire90@gmail.com>
2014-09-12 18:33:09 -0700

    Added Color to bar graph and added y-axis labels

--- cs315-hwk1/grapher.js
+++ cs315-hwk1/grapher.js
@@ -1,3 +1,14 @@
+/*
+  Assignment #1: Grapher
+
+  Casey McGuire
+  CS315: Computer Graphics, Fall 2014
+  9/12/2014
+  University of Puget Sound
+  
+*/
+
+
 var datatable = 'assets/pixar.txt'; //the file to load
 //var file;//for the file to be loaded
 var graphs;//holds formatted graph data
@@ -16,7 +27,7 @@ function init() {
     console.log(file);//print to developer console
     console.log(typeof file);
     
-    //create a method to help 
+    //create a method to help parse file
     file.getData = function(){
 	var a = new Array();
 	for(var x = 2; x < file.length; x++){
@@ -29,17 +40,11 @@ function init() {
 	}
 	return a;
     }
-    
     graphs = {
-	
 	title :file[1],
 	data : file.getData(),
 	formats: file[0].split(",")
     }
-    
-    console.log(graphs);
-    
-    
 }
 
 
@@ -47,17 +52,15 @@ function init() {
 function draw() {
     
     context.strokeStyle = "#000000";
-    context.stroke();
+console.log(graphs);
     
+    //Writes a title for our graphs
     context.font = "bold 12px sans-serif";
     context.fillText(graphs.title , canvas.width/3, 43);
 
-    singleGraphArea = canvas.width/3;
+    singleGraphArea = canvas.width/graphs.formats.length;
     
-    //  console.log("single graph area" + singleGraphArea);
-    //  console.log("graph width" + canvas.width);
-    //  console.log("graph height" + canvas.height);
-    //draw our graphs
+    //draw each of our graphs in 1/3 of the canvas
     drawLineGraph(0, singleGraphArea);
     drawBarGraph(singleGraphArea, 2*singleGraphArea);
     drawPieChart(2*singleGraphArea, 3*singleGraphArea);
@@ -69,7 +72,6 @@ function drawSeparator(xTo){
     for(var x = xTo; x<xTo*graphs.formats.length; x+= xTo){
         context.moveTo(x, 0);
         context.lineTo(x, canvas.height);
-	//  console.log(x);
     }
 }
 
@@ -77,15 +79,14 @@ function drawSeparator(xTo){
 function getGraphDimensions(begin, end){
     var sideBuffer = (end - begin) *.07;
     
+    //put some space between each graph (for labels and such)
     begin += sideBuffer;
     end -= sideBuffer;
     
+    //calculate buffers so the graph is roughly in the middle
+    //of its allocated portion of the canvas
     var bottomBuffer = canvas.height*.85;
     var topBuffer = canvas.height/6;
-
-    // console.log("bottom buffer " + bottomBuffer);
-    //  console.log("top buffer " + topBuffer);
-    
     var topOfGraph = canvas.height - topBuffer;
     var bottomOfGraph = canvas.height - bottomBuffer;
     
@@ -102,10 +103,8 @@ function getGraphDimensions(begin, end){
 
 function drawLineGraph(begin, end){
     context.beginPath();
-    
-    //create a buffer of white space between the 
-    //graph and the edges of the allocated graph
-    //space
+
+    //get the dimensions for our graph
     var dimensions = getGraphDimensions(begin, end);
     
     begin = dimensions.begin;
@@ -115,28 +114,23 @@ function drawLineGraph(begin, end){
     var topBuffer = dimensions.topBuffer;
     var topOfGraph = dimensions.topOfGraph;
     var bottomOfGraph = dimensions.bottomOfGraph;    
+
     
-    //first draw the axes
-    drawAxes(begin, end, topOfGraph, bottomOfGraph);
-    
+    //get some other values that will help draw the graph
+    var lineWidth = (end - begin)/graphs.data.length;
     var increment = getLength(begin, end)/graphs.data.length;
-    
-    //console.log(begin);
-    // console.log(end);
-    
-    //now draw the graph
-    var lineWidth = (end - begin)/graphs.data.length 
-    
+
     //find the largest and smallest values for linear interpolation
     var smallest = findSmallestValue();
     var largest = findLargestValue();
-    
-    //  console.log("largest " + largest + " smallest " + smallest);
-    
     var height = topOfGraph - bottomOfGraph;
 
-    //draw the graph
+    //first draw the axes
+    drawAxes(begin, end, topOfGraph, bottomOfGraph);
+    drawLabels(begin,lineWidth);
     
+
+    //now draw the graph
     var x;
     var y;
     for(var i = 1; i <= graphs.data.length ; i++){
@@ -145,13 +139,9 @@ function drawLineGraph(begin, end){
 	if(i == 1) context.moveTo(x, y);
 	context.lineTo(x, y);
 	context.arc(x, y, 1, 0, 2*Math.PI, false);//put circles at each point
-	
     }
     context.stroke();
     context.closePath();
-    
-    drawLabels(begin,lineWidth);
-    
 }
 
 //find the smallest value in the graphs.data array
@@ -182,6 +172,7 @@ function findLargestValue(){
 function drawAxes(begin, end, topOfGraph, bottomOfGraph){
     
     context.beginPath();
+
     //draw x axis
     context.moveTo(begin, topOfGraph);
     context.lineTo(end, topOfGraph);
@@ -189,9 +180,19 @@ function drawAxes(begin, end, topOfGraph, bottomOfGraph){
     //draw y axis
     context.moveTo(begin, bottomOfGraph);
     context.lineTo(begin, topOfGraph);
+
+    context.closePath();
+    context.stroke();
     
-    //create divots along x axis
-    context.moveTo(begin, topOfGraph);
+    //create divots along x and y axis
+    drawScaledXAxis(begin, end, topOfGraph);
+    drawScaledYAxis(begin,end,topOfGraph, bottomOfGraph);
+   
+}
+
+function drawScaledXAxis(begin, end, topOfGraph){
+    context.beginPath();
+    context.moveTo(begin,topOfGraph);
     var increment = getLength(begin,end)/graphs.data.length;
     for(var i = 0; i<graphs.data.length; i++){
         context.moveTo(i*increment + begin, topOfGraph);
@@ -200,20 +201,22 @@ function drawAxes(begin, end, topOfGraph, bottomOfGraph){
     context.stroke();
     context.closePath();
 
-    drawScaledYAxis(begin,end,topOfGraph, bottomOfGraph);
 }
 
+//This function draws divots on the Y-axis to indicate the scale
 function drawScaledYAxis(begin, end, topOfGraph, bottomOfGraph){
     context.beginPath();
    
     var yAxisLength = getLength(bottomOfGraph, topOfGraph);
     console.log("y axis" + yAxisLength);
     var increment = yAxisLength/graphs.data.length;
-    
+    var smallest = findSmallestValue();
+    var labelIncrementer = getLength(begin, end)/graphs.data.length;
+
     for(var i = 0; i < graphs.data.length; i++){
 	context.moveTo(begin,topOfGraph-i*increment);
 	context.lineTo(begin-4,topOfGraph-i*increment);
-
+	context.fillText(Math.round(smallest+ labelIncrementer*i), begin - 15, topOfGraph-i*increment, 15);
     }
 
     context.stroke();
@@ -223,9 +226,6 @@ function drawScaledYAxis(begin, end, topOfGraph, bottomOfGraph){
 //draws appropriate labels beneath graphs
 function drawLabels(begin, spacing, topOfGraph){
     context.save();
-    console.log("begin" + begin);
-    
-    
     context.translate(begin, canvas.height);
     context.rotate(-Math.PI/2);
     
@@ -233,10 +233,10 @@ function drawLabels(begin, spacing, topOfGraph){
     for(var i = 0; i< graphs.data.length; i++){	
 	context.fillText(graphs.data[i].label , 20, spacing-3, 80);
 	spacing += x;
-	
     }
     context.restore();
 }
+
 
 
 //returns the length between the begin point and the end point
@@ -246,6 +246,7 @@ function getLength(begin, end){
 
 function drawBarGraph(begin, end){
     
+    //get some dimensions that will help properly draw scaled graph
     var dimensions = getGraphDimensions(begin, end);    
     begin = dimensions.begin;
     end = dimensions.end;
@@ -254,47 +255,48 @@ function drawBarGraph(begin, end){
     var topBuffer = dimensions.topBuffer;
     var topOfGraph = dimensions.topOfGraph;
     var bottomOfGraph = dimensions.bottomOfGraph; 
-    
-    drawAxes(begin, end, topOfGraph, bottomOfGraph);
-    
-    
+
+    //calculate the width of single bar in the graph
+    //such that the bars cover the entire x-axis
     var barWidth = getLength(begin,end)/graphs.data.length;
     
+
+    //The largest and smallest are for linear interpolation of the bars
     var smallest = findSmallestValue();
     var largest = findLargestValue();
     var height = topOfGraph - bottomOfGraph;
     
-    
+    //draw our axes and labels
+    drawAxes(begin, end, topOfGraph, bottomOfGraph);
+    drawLabels(begin, barWidth);
+
+    //get a palette of colors, one for each bar
+    var colors = generateColors(graphs.data.length);
+
+    //draw the bar graph
+    context.save();
     for(var i = 0; i< graphs.data.length; i++){
-	//var y = (graphs.data[i].value/largest)*height + topBuffer;
 	var y = (-((graphs.data[i].value - smallest)/(largest - smallest))*height + topOfGraph)*.995;
 	var barHeight = topOfGraph - y;
-	
-	//	context.fillRect(i*barWidth + begin,topOfGraph - barHeight, barWidth, barHeight);
-	//var height = bottomOfGraph - y;
-	//	console.log("height " + height);
-	console.log(topOfGraph);
+	context.fillStyle = colors[i].getHSLString();
 	context.fillRect(i*barWidth + begin,y , barWidth, barHeight);
-	
-	
-	
     }
-    console.log("bar width" + barWidth);
-    drawLabels(begin, barWidth);
+    context.restore();
 }
 
-//draw the pie chart in eh 
+//draws the pie chart between the begin and end parameters on the x-axis
 function drawPieChart(begin, end){
     
-    console.log("begin " + begin + " end "+ end);
+   
     
+    //calculate the x and y values at the center of the circle
     var centerX = begin + (end - begin)/2;
     var centerY = canvas.height/2;
 
     context.beginPath();
     context.moveTo(centerX,centerY);
-   // console.log("Center x " + centerX);
-
+  
+    
     var wholeCircle = 2*Math.PI;
     var total = getTotal();
     var curAngle = 0;
@@ -302,10 +304,11 @@ function drawPieChart(begin, end){
 
     
     var fractionOfTotal;
+    var colors = generateColors(graphs.data.length);
     for(var i = 0; i < graphs.data.length ; i++){
 	fractionOfTotal = graphs.data[i].value/total;
 	curAngle += wholeCircle*fractionOfTotal;
-	drawSingleSlice(centerX,centerY, prevAngle, curAngle, 100);
+	drawSingleSlice(centerX,centerY, prevAngle, curAngle, 100, colors[i]);
 	prevAngle = curAngle;
     }
 
@@ -313,29 +316,55 @@ function drawPieChart(begin, end){
 
 }
 
-function drawSingleSlice(centerX, centerY,prevAngle, curAngle, radius){
+function drawSingleSlice(centerX, centerY,prevAngle, curAngle, radius, color){
     context.beginPath();
+
     context.moveTo(centerX, centerY);
     context.arc(centerX, centerY, radius, prevAngle, curAngle, false);
     context.lineTo(centerX, centerY);
-    context.fillStyle = 'green';
+    context.fillStyle = color.getHSLString();
     context.stroke();
+
     context.closePath();
     context.fill();
 }
 
+//returns an array containing a palette of colors
+function generateColors(numColors){
+    colors = new Array();
+    
+    var hue;
+    var saturation;
+    var brightness;
+    var fraction = 360/graphs.data.length;
+    
+    for(var i = 0; i < numColors; i++){
+	
+	hue = fraction*i;
+	saturation = 85;
+	brightness = 50;
+	colors[colors.length] = {
+	    hue: hue,
+	    saturation: saturation,
+	    brightness: brightness
+	}
+	colors[colors.length-1].getHSLString = function(){
+	    return "hsl(" + this.hue + "," + this.saturation + "%," + this.brightness + "%)"; 
+	}
+    }
+
+    return colors;
+
+}
+
 //the total of the value entries
 function getTotal(){
-    
     var total = 0;
     for(var i = 0; i < graphs.data.length; i++){
 	total += graphs.data[i].value;
     }
     return total;
-    
 }
-
-
 
 //once html has loaded, set up and draw on the canvas
 $(document).ready(function(){
